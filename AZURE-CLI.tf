ac4193466892d5ca7d41409463df14bcee6a0a2a
nikki <nikhilkumar8686.nk@gmail.com>
2023-10-02 11:00:08 +0530

    day 1 terraform authenticate azure portal

---
+++ AZURE-CLI.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "example"
+  location = "West Europe"
+}
